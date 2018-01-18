64539ea15f89bfe7affb36b61bab8b34e2a70de3
kailsar <c.g.burnett@gmail.com>
2018-01-18 09:17:20 +0000

    First Commit

---
+++ ShrinkJPG.ps1
@@ -0,0 +1,22 @@
+﻿$argumentStem = "mogrify -resize 1920x1080 `""
+
+function recurseDirectory($path)
+{
+  $fc = new-object -com scripting.filesystemobject
+  $folder = $fc.getfolder($path)
+
+  foreach ($i in $folder.files) {
+  if ($i.size -gt 1537152 -And $i.type -eq "JPG File")
+    {
+	$arguments = $argumentStem + $i.path + "`""  
+    Write-Output $arguments
+    Start-Process -FilePath "C:\Program Files\ImageMagick-7.0.7-Q16\magick.exe" -ArgumentList $arguments -NoNewWindow
+    }
+  }
+
+  foreach ($i in $folder.subfolders) {
+    recurseDirectory($i.path)
+  }
+}
+
+
